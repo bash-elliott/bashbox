5b8125d62010b2eed8ced91f4885ac2c21f897ed
Bash Elliott <spicethings9@gmail.com>
2022-03-24 13:51:40 +1100

    begun development on two column box

--- textbox.py
+++ textbox.py
@@ -1,16 +1,13 @@
-# Generates a dynamically sized box to hold text. Can take an unlimited number of string arguments. 
-def startBox(*text):
-    # The function takes in arguments using the * operator, which enters all of the arguments into a tuple. This function doesn't work with correctly with tuples, so this line of code converts it into a list.
-    text = list(text)
+# Generates a dynamically sized box to hold text.
+def simpleBox(text = []):
 
-    # Initialize the max variable, which is later used to determine the wideness of the resulting box.
+    # Initialize max variable
     max = 0
 
-    # Loops through every item in the list of strings, and sets max to the highest number AKA the length of the longest string.
-    # Note: Use this code to make the max function later on.
+    # Length of longest string
     for i in range(len(text)):
-        if len(text[i]) > max:
-            max = len(text[i])
+        if len(str(text[i])) > max:
+            max = len(str(text[i]))
 
     # Sets the characters for the box's corners (cornerChars) and walls (edgeCharV and edgeCharH, corresponding to vertical wall and horizontal wall respectively).
     # cornerChars works as an array: top left [0], top right [1], bottom left [2], bottom right [3]
@@ -25,7 +22,79 @@ def startBox(*text):
 
     # Loops through every string in the text list and prints it inside the box.
     for i in range(len(text)):
-        print(edgeCharV + " " + text[i] + (" " * (max - len(text[i]) + 1)) + edgeCharV)
+        print(edgeCharV + " " + str(text[i]) + (" " * (max - len(str(text[i])) + 1)) + edgeCharV)
 
     # Prints the bottom row of the box.
     print(cornerChars[2] + (edgeCharH * (max + 2)) + cornerChars[3] )
+
+def twoColBox(textA = [], textB = []):
+    textA = list(textA)
+    textB = list(textB)
+
+    maxA = 0
+    maxB = 0
+
+    for i in range(len(textA)):
+        if len(str(textA[i])) > maxA:
+            maxA = len(str(textA[i]))
+    
+    for i in range(len(textB)):
+        if len(str(textB[i])) > maxB:
+            maxB = len(str(textB[i]))
+
+    rowMax = max([len(textA), len(textB)])
+
+    # Sets the characters for the box's corners (cornerChars) and walls (edgeCharV and edgeCharH, corresponding to vertical wall and horizontal wall respectively).
+    # cornerChars works as an array: top left [0], top right [1], bottom left [2], bottom right [3]
+    # splitChars works as an array: top split [0], bottom split [1],
+    # Can take in any unicode character, but works best with the unicode character codes.
+    # \u2554 = ╔
+    cornerChars = ["\u2554", "\u2557", "\u255a", "\u255d"]
+    splitChars = ["\u2566", "\u2569"]
+    edgeCharV = "\u2551"
+    edgeCharH = "\u2550"
+
+    # Prints the top row of the box.
+    print(cornerChars[0] + (edgeCharH * (maxA + 2)) + splitChars[0] + (edgeCharH * (maxB + 2)) + cornerChars[1] )
+
+    # Loops through every string in the text list and prints it inside the box.
+    for i in range(rowMax):
+        try:
+            currentTextA = str(textA[i])
+        except:
+            currentTextA = ""
+
+        try:    
+            currentTextB = str(textB[i])
+        except:
+            currentTextB = ""
+
+        print(
+            edgeCharV + 
+            " " + 
+            currentTextA + 
+            (
+                " " * (maxA - len(currentTextA) + 1)
+            ) + 
+            edgeCharV + 
+            " " + 
+            currentTextB + 
+            (
+                " " * (maxB - len(currentTextB) + 1)
+            ) + 
+            edgeCharV 
+        )
+    
+    # Prints the bottom row of the box.
+    print(cornerChars[2] + (edgeCharH * (maxA + 2)) + splitChars[1] + (edgeCharH * (maxB + 2)) + cornerChars[3] )
+
+twoColBox(
+    [
+        "Name", 
+        "Age", 
+        "Sex"
+    ], 
+    [
+        "Bash Edward Elliott", 
+        "17", 
+        "Male"])
