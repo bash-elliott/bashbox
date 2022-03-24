d846ba4b016a7f4a27657452e29ddec25f586ea4
Bash Elliott <spicethings9@gmail.com>
2022-03-25 09:22:48 +1100

    added title functionality via setTitle(string).
    doesn't play well when there's only one column and it's smaller than the title length. gonna have to fix that.

--- textbox.py
+++ textbox.py
@@ -16,6 +16,8 @@ class TextBox:
     def __init__(self):
         self.columns = 1
         self.text = [[]] * 1
+        self.title = ""
+        self.useTitle = False
 
     def setColumns(self, num):
         """
@@ -25,6 +27,13 @@ class TextBox:
         """
         self.columns = num
         self.text = [[]] * num
+
+    def setTitle(self, title):
+        self.title = title
+        if title != "":
+            self.useTitle = True
+        else:
+            self.useTitle = False
 
     def setText(self, col, *text):
         """
@@ -44,48 +53,81 @@ class TextBox:
         texts = list(self.text)
         maxes = []
         currentTexts = []
-        # for i in range(len(inputs)):
-        #     texts.append(list(inputs[i]))
         
+        # Sets the maxes array for all given lines.
         for i in range(len(texts)):
             maxes.append(0)
             for j in range(len(texts[i])):
                 if len(str(texts[i][j])) > maxes[i]:
                     maxes[i] = len(str(texts[i][j]))
 
+        # Gets the maximum number of lines to draw based off the longest array.
         rowMax = len(max(texts))
 
-        t = CornerTL
-        for i in range(len(maxes)):
-            t += EdgeH * (maxes[i] + 2)
-            if i < len(maxes) - 1:
-                t += SplitU
-        t += CornerTR
+        titleArray = []
+        if self.useTitle:
+            spaces = sum(maxes) + ((len(maxes) - 1) * 2) + (self.columns + 2) - len(self.title) - 2
+            if spaces <= 0:
+                spaces = 1
+            titleLine = CornerTL
+            titleLine += EdgeH * (spaces + len(self.title) + 1) 
+            titleLine += CornerTR
+            titleArray.append(titleLine)
+            titleLine = EdgeV
+            titleLine += " " + self.title + " " * spaces
+            titleLine += EdgeV
+            titleArray.append(titleLine)
 
-        mA = []
+        # Generate the top part of the textbox.
+        if self.useTitle:
+            topleft = SplitL
+            topright = SplitR
+        else:
+            topleft = CornerTL
+            topright = CornerTR
+        topLine = topleft
+        for i in range(len(maxes)):
+            topLine += EdgeH * (maxes[i] + 2)
+            # If this isn't the last column, add a split.
+            if i < len(maxes) - 1:
+                topLine += SplitU
+        topLine += topright
+
+        # Generate the central part of the textbox.
+        centralArray = []
         for i in range(rowMax):
-            m = ""
+            middleString = ""
             for j in range(len(texts)):
+                # Try getting the text from the index. If exception is caught, use empty string.
                 try:
                     currentTexts.append(str(texts[j][i]))
                 except:
                     currentTexts.append("")
-            m += EdgeV
+            middleString += EdgeV
+            # Draw the text in each column.
             for j in range(len(currentTexts)):
-                m += " " + currentTexts[j] + (" " * (maxes[j] - len(currentTexts[j]) + 1))
-                m += EdgeV
+                middleString += " " + currentTexts[j] + (" " * (maxes[j] - len(currentTexts[j]) + 1))
+                middleString += EdgeV
             currentTexts = []
-            mA.append(m)
-        m += EdgeV
+            # Add the current string to the final array.
+            centralArray.append(middleString)
+        # Print the last split.
+        middleString += EdgeV
 
-        b = CornerBL
+        # Generate the bottom part of the textbox.
+        bottomLine = CornerBL
         for i in range(len(maxes)):
-            b += EdgeH * (maxes[i] + 2)
+            bottomLine += EdgeH * (maxes[i] + 2)
+            # If this isn't the last column, add a split.
             if i < len(maxes) - 1:
-                b += SplitD
-        b += CornerBR
+                bottomLine += SplitD
+        bottomLine += CornerBR
 
-        print(t)
-        for i in range(len(mA)):
-            print(mA[i])
-        print(b)
+        # Print the textbox.
+        if self.useTitle:
+            for i in range(len(titleArray)):
+                print(titleArray[i])
+        print(topLine)
+        for i in range(len(centralArray)):
+            print(centralArray[i])
+        print(bottomLine)
