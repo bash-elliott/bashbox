00cacc41f0d6e2d25459cf4341548257be54676f
Bash Elliott <spicethings9@gmail.com>
2022-03-29 13:22:27 +1100

    pushing and releasing v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bashbox",
-    version="0.0.1",
+    version="1.0.0",
     author="Bash Elliott",
     author_email="spicethings9@gmail.com",
     description="Bash's textbox package.",
