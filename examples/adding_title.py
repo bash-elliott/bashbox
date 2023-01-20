7599437f4df8de94aff11420c5ae6fa87ce77889
Bash Elliott <bashelliott@gmail.com>
2023-01-20 12:31:15 +1100

    revamp 20th Jan 2023

--- examples/adding_title.py
+++
@@ -1,6 +0,0 @@
-from bashbox import bashbox
-
-box = bashbox()
-box.setTitle("Look, this is a title!")
-box.setText(0, "There's a title up there!")
-box.draw()
--- examples/multiple_columns.py
+++
@@ -1,7 +0,0 @@
-from bashbox import bashbox
-
-box = bashbox()
-box.setColumns(2)
-box.setText(0, "Some Text!")
-box.setText(1, "Another column of text!")
-box.draw()
